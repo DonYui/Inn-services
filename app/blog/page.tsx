14145b73df87b6ddff4add57449ea7d5af1f2783
lissoguillen-ui <lisso.guillen@gmail.com>
2026-02-11 15:31:55 -0600

    cambios que hay en mi

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+export default function Blog() {
+  return (
+    <iframe
+      src="/blog.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        display: 'block',
+      }}
+    />
+  )
+}

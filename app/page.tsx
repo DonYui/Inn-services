be4d1cd55e5867899555c29e15623e5e8654e41e
lissoguillen-ui <lisso.guillen@gmail.com>
2026-02-12 13:14:51 -0600

    INN SERVICES
    Blog y servicios

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-export default function Home() {
-  return (
-    <iframe
-      src="/index.html"
-      style={{
-        width: '100%',
-        height: '100vh',
-        border: 'none',
-        display: 'block',
-      }}
-    />
-  )
+export default function Page() {
+  return null
 }

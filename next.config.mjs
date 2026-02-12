e5190310507cd22f3af81c3b9bfd93805d703b30
Fernando Sánchez <161346854+DonYui@users.noreply.github.com>
2026-02-12 15:00:37 -0600

    Revert "foto inicio"
    This reverts commit 6b733763a850825922890a614a381fb596c3908f.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,25 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/',
+          destination: '/index.html',
+        },
+        {
+          source: '/servicios',
+          destination: '/servicios.html',
+        },
+        {
+          source: '/blog',
+          destination: '/blog.html',
+        },
+      ],
+    }
+  },
 }
 
 export default nextConfig

6b733763a850825922890a614a381fb596c3908f
lissoguillen-ui <lisso.guillen@gmail.com>
2026-02-12 14:46:44 -0600

    foto inicio

--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,7 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/',
-          destination: '/index.html',
-        },
-        {
-          source: '/servicios',
-          destination: '/servicios.html',
-        },
-        {
-          source: '/blog',
-          destination: '/blog.html',
-        },
-      ],
-    }
-  },
+  }
 }
 
 export default nextConfig
